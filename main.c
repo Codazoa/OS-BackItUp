54ad66e1d2d6fde1bdea185f6d537da48d86bae4
codazoa <codazoa@protonmail.com>
2023-04-11 13:00:08 -0700

    Argument parsing finished

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 #include "options.h"
 
@@ -9,7 +10,36 @@
 // spawn a new thread for every file in the directory to copy each one into .backup
 // go down a folder
 
-int main(int argc, char const *argv[]){
+// parse command line arguments
+// return 0 for regular backup, 1 for restore
+int parseArgs(int argc, char const *argv[]);
 
+void runBackup();
+
+void runRestore();
+
+int main(int argc, char const *argv[]){
+    if (parseArgs(argc, argv) == 0) {
+        runBackup();
+    } else {
+        runRestore();
+    }
     return 0;
+}
+
+int parseArgs(int argc, char const *argv[]){
+    if (argc > 1 && strcmp(argv[1], "-r") == 0) return 1;
+    else if (argc > 1 && strcmp(argv[1], "-r") != 0) {
+        fprintf(stderr, "Invalid option '%s'\n", argv[1]);
+        exit(1);
+    }
+    return 0;
+}
+
+void runBackup(){
+    if (DEBUG) printf("Backup\n");
+}
+
+void runRestore(){
+    if (DEBUG) printf("Restore\n");
 }
