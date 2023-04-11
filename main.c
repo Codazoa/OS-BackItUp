937bdc2e048b8a61daadcaec1e0852852289ba1c
codazoa <codazoa@protonmail.com>
2023-04-11 14:51:58 -0700

    Update argument parser

--- main.c
+++ main.c
@@ -11,35 +11,46 @@
 // go down a folder
 
 // parse command line arguments
-// return 0 for regular backup, 1 for restore
-int parseArgs(int argc, char const *argv[]);
+void parseArgs(int argc, char const *argv[], int *operation, char const **src_path);
 
-void runBackup();
+void runBackup(char const *src_path);
 
-void runRestore();
+void runRestore(char const *src_path);
 
 int main(int argc, char const *argv[]){
-    if (parseArgs(argc, argv) == 0) {
-        runBackup();
-    } else {
-        runRestore();
-    }
-    return 0;
-}
 
-int parseArgs(int argc, char const *argv[]){
-    if (argc > 1 && strcmp(argv[1], "-r") == 0) return 1;
-    else if (argc > 1 && strcmp(argv[1], "-r") != 0) {
-        fprintf(stderr, "Invalid option '%s'\n", argv[1]);
+    char const *src_path = NULL; // path to directory to backup or restore
+    int operation = 0; // 1 for restore, 0 for backup
+
+    parseArgs(argc, argv, &operation, &src_path);
+
+    if (src_path == NULL) {
+        fprintf(stderr, "Error: Directory path not specified\n");
         exit(1);
     }
+
+    if (DEBUG) printf("Operation %d, Directory Path: %s\n", operation, src_path);
+
+    if (operation == 0) {
+        runBackup(src_path);
+    } else {
+        runRestore(src_path);
+    }
+
     return 0;
 }
 
-void runBackup(){
-    if (DEBUG) printf("Backup\n");
+void parseArgs(int argc, char const *argv[], int *operation, char const **src_path){
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-r") == 0) *operation = 1;
+        else *src_path = argv[i];
+    }
 }
 
-void runRestore(){
-    if (DEBUG) printf("Restore\n");
+void runBackup(char const *src_path){
+    if (DEBUG) printf("Backup: %s\n", src_path);
+}
+
+void runRestore(char const *src_path){
+    if (DEBUG) printf("Restore: %s\n", src_path);
 }
