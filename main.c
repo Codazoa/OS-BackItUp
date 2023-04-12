b5a0964e68ad007bbbb147a1687424d6e5212843
codazoa <codazoa@protonmail.com>
2023-04-11 22:24:22 -0700

    Clean up code

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 // go down a folder
 
 typedef struct {
+    char *path;
     int num_files;
     int num_dirs;
     char **file_list;
@@ -26,7 +27,7 @@ typedef struct {
 // parse command line arguments
 void parseArgs(int argc, char const *argv[], int *operation);
 
-Dir_info_t *getDirInfo(DIR *dir);
+Dir_info_t *getDirInfo(char *path);
 
 void runBackup(char *path);
 
@@ -63,18 +64,10 @@ void parseArgs(int argc, char const *argv[], int *operation){
 }
 
 void runBackup(char *path){
-    DIR *dir;
     if (DEBUG) printf("\nBackup: %s\n", path);
 
-    // open this directory
-    dir = opendir(path);
-    if (dir == NULL) {
-        perror("backitup Error");
-        exit(1);
-    }
-
-    Dir_info_t *dir_info = getDirInfo(dir);
-
+    // get directory information
+    Dir_info_t *dir_info = getDirInfo(path);
 
     // if no backup directory was found, create it
     if (!dir_info->has_backup_dir) {
@@ -126,18 +119,67 @@ void runBackup(char *path){
 }
 
 void runRestore(char *path){
-    DIR *dir = NULL;
+    char new_bak_path[PATH_MAX];
+    sprintf(new_bak_path, "%s/%s", path, ".backup");
+
+    if (DEBUG) printf("Restore: %s\n", new_bak_path);
+
+    Dir_info_t *dir_info = getDirInfo(path);
+
+    // iterate through directories
+    for (int i = 0; i < dir_info->num_dirs; i++) {
+        if (DEBUG) printf("%s [directory]\n", dir_info->dir_list[i]);
+        // recursively go down another folder
+        char new_path[PATH_MAX];
+        sprintf(new_path, "%s/%s", path, dir_info->dir_list[i]);
+        runRestore(new_path);
+    }
+    free(dir_info->dir_list);
+
+    // if there is not a backup directory
+    if (!dir_info->has_backup_dir) {
+        return;
+    }
+
+    // get info on backup directory that exists
+    Dir_info_t *backup_info = getDirInfo(new_bak_path);
+
+    pthread_t threads[backup_info->num_files];
+    Copy_args_t copy_args[backup_info->num_files];
+    // iterate through files in backup
+    for (int i = 0; i < backup_info->num_files; i++) {
+        if (DEBUG) printf("%s/%s [file]\n", backup_info->path, backup_info->file_list[i]);
+        // spawn off a thread for the file
+        copy_args[i].path = backup_info->path;
+        copy_args[i].file_name = backup_info->file_list[i];
+
+        if (pthread_create(&threads[i], NULL, restore, &copy_args[i]) != 0 ) {
+            perror("Error creating thread");
+        }
+    }
+
+    // join the threads
+    for (int i = 0; i < backup_info->num_files; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    free(backup_info->file_list);
+
+
+    // base case when there are no more folders 
+    if (dir_info->num_dirs == 0) {
+        return;
+    }
+
+}
+
+Dir_info_t *getDirInfo(char *path) {
+    DIR *dir;
     struct dirent *entry = NULL;
     int has_backup_dir = 0;
     int num_files = 0;
     int num_dirs = 0;
     char **dir_list = NULL;
     char **file_list = NULL;
-
-    char new_bak_path[PATH_MAX];
-    sprintf(new_bak_path, "%s/%s", path, ".backup");
-
-    if (DEBUG) printf("Restore: %s\n", new_bak_path);
 
     // open this directory
     dir = opendir(path);
@@ -145,99 +187,6 @@ void runRestore(char *path){
         perror("backitup Error");
         exit(1);
     }
-
-    // analyze the directory information
-    // check if .backup exists
-    // add directories to dir_list
-    // add files to file_list
-    while ((entry = readdir(dir)) != NULL) {
-        // sprintf(name, "%s", entry->d_name);
-        if (entry->d_type == DT_DIR) {
-            // is a directory
-
-            // don't add the . or .. directories to the list
-            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-                continue;
-            }
-
-            // does the current directory have a .backup folder
-            if (strcmp(entry->d_name, ".backup") == 0) {
-                has_backup_dir = 1;
-            } else {
-                // increment number of directories and add directory name onto dir_list
-                num_dirs++;
-                dir_list = realloc(dir_list, num_dirs * sizeof(char *));
-                dir_list[num_dirs - 1] = strdup(entry->d_name);
-            }
-
-        } else {
-            // is a file
-            if (DEBUG) printf("%s\n", entry->d_name);
-
-            // increment number of files and add file name onto file_list
-            num_files++;
-            file_list = realloc(file_list, num_files * sizeof(char *));
-            file_list[num_files - 1] = strdup(entry->d_name);
-        }
-    }
-    closedir(dir);
-
-    // if no backup directory was found, create it
-    if (!has_backup_dir) {
-        mode_t mode = S_IRWXU | S_IRWXG | S_IRWXO;
-        char backup_path[PATH_MAX];
-        sprintf(backup_path, "%s/%s", path, ".backup");
-        int result = mkdir(backup_path, mode);
-
-        if (result == 0){
-            printf(".backup created successfully.\n");
-        } else {
-            perror("Error: Unable to create .backup\n");
-        }
-    }
-
-    // iterate through directories
-    for (int i = 0; i < num_dirs; i++) {
-        if (DEBUG) printf("%s [directory]\n", dir_list[i]);
-        // recursively go down another folder
-        char new_path[PATH_MAX];
-        sprintf(new_path, "%s/%s", path, dir_list[i]);
-        runBackup(new_path);
-    }
-    free(dir_list);
-
-    pthread_t threads[num_files];
-    Copy_args_t copy_args[num_files];
-    // iterate through files
-    for (int i = 0; i < num_files; i++) {
-        if (DEBUG) printf("%s/%s [file]\n", path, file_list[i]);
-        // spawn off a thread for the file
-        copy_args[i].path = path;
-        copy_args[i].file_name = file_list[i];
-        if (pthread_create(&threads[i], NULL, backup, &copy_args[i]) != 0 ) {
-            perror("Error creating thread");
-        }
-    }
-
-    // join the threads
-    for (int i = 0; i < num_files; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    free(file_list);
-
-    // base case when there are no more folders 
-    if (num_dirs == 0) {
-        return;
-    }
-}
-
-Dir_info_t *getDirInfo(DIR *dir) {
-    struct dirent *entry = NULL;
-    int has_backup_dir = 0;
-    int num_files = 0;
-    int num_dirs = 0;
-    char **dir_list = NULL;
-    char **file_list = NULL;
 
     // analyze the directory information
     // check if .backup exists
@@ -276,6 +225,7 @@ Dir_info_t *getDirInfo(DIR *dir) {
     closedir(dir);
 
     Dir_info_t *return_info = malloc(sizeof(Dir_info_t));
+    return_info->path = path; 
     return_info->num_files = num_files;
     return_info->num_dirs = num_dirs;
     return_info->file_list = file_list;
